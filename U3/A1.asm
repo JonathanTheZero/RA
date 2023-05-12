5a6dd9029a904af31b4d54248001baad489e6541
JonathanTheZero <jonathan.frohne@web.de>
2023-05-12 12:58:26 +0200

    Assembly Horror

---
+++ U3/A1.asm
@@ -0,0 +1,7 @@
+li $t0, 5
+li $t1, 10
+li $t2, 35
+addi $sp, $sp, -20
+sw $t1, 4($sp)
+sw $t0, 12($sp)
+sw $t2, 8($sp)
--- U3/simple-counter.s
+++ U3/simple-counter.s
@@ -2,26 +2,26 @@
 sMsg:   .asciiz "\nAktueller Wert:\t: "	# FÃ¼r Ergebnisanzeige
 
 .text
-main:	li 	$t0, 	10		# Kommentar 1 Nr.:
+main:	li 	$t0, 	10		# Kommentar 1 Nr.: i := 10
 
-while:	blez	$t0,	elihw		# Kommentar 2 Nr.:
-	addi	$t0,	$t0,	-1	# Kommentar 3 Nr.:
-	addi	$sp,	$sp,	-4	# Kommentar 4 Nr.:
-	sw	$t0,	4($sp)		# Kommentar 5 Nr.:
-	move	$a0,	$t0		# Kommentar 6 Nr.:
+while:	blez	$t0,	elihw		# Kommentar 2 Nr.: while i > 0
+	addi	$t0,	$t0,	-1	# Kommentar 3 Nr.: i := i - 1
+	addi	$sp,	$sp,	-4	# Kommentar 4 Nr.: Schaffe Platz auf Stack
+	sw	$t0,	4($sp)		# Kommentar 5 Nr.: Sichere i auf Stack
+	move	$a0,	$t0		# Kommentar 6 Nr.: Übergebe Argument
 	jal	print			# Unterprozeduraufruf
-	lw	$t0,	4($sp)		# Kommentar 7 Nr.:
-	addi	$sp,	$sp,	4	# Kommentar 8 Nr.:
+	lw	$t0,	4($sp)		# Kommentar 7 Nr.: Lade i vom Stack
+	addi	$sp,	$sp,	4	# Kommentar 8 Nr.: Setze Stackgröße zurück
 	b	while			# Schleife wiederholen
 elihw: 
 	li $v0,	10			
 	syscall				# EXIT des Programms
 print:
-	move	$t0,	$a0		# Kommentar 9 Nr.:
+	move	$t0,	$a0		# Kommentar 9 Nr.: Sichere übergebenes Argument
 	li 	$v0,	4		# print_string
 	la	$a0, sMsg		
 	syscall
-	move	$a0,	$t0		# Kommentar 10 	Nr.:
+	move	$a0,	$t0		# Kommentar 10 	Nr.: Übergebe Argument
 	li 	$v0,	1 		# print_int
 	syscall
 	jr $ra				# RÃ¼cksprung
